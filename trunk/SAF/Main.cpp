0503ab011ff68b6fe2ac93bacc84286442d82dda
radomir@mazon.pl <radomir@mazon.pl@9dec7959-b12e-a28f-30bc-18173cd9f915>
2011-10-03 11:44:01 +0000

    Projekt testowy

---
+++ trunk/SAF/Main.cpp
@@ -0,0 +1,34 @@
+/**
+ *
+ */
+
+#include "avr/interrupt.h"
+#include "avrcore.h"
+#include "Event.h"
+#include "Test.h"
+
+int main()
+{
+	/**
+	 * Tworzenie obiektow
+	 */
+
+
+	Test test;
+
+	/**
+	 * Konfiguracja
+	 */
+	Clock::get()->add(&test, 10);
+
+	/**
+	 * Main. Gowna petla przekazuje steroanie do SAF.
+	 * Nie nalezy jej zmieniac...
+	 */
+	sei();
+	while(1) {
+		Clock::get()->process();
+		EventBus::get()->process();
+	}
+}
+
---
+++ trunk/SAF/Test.cpp
@@ -0,0 +1,12 @@
+/*
+ * Test.cpp
+ *
+ *  Created on: 03-10-2011
+ *      Author: radomir.mazon
+ */
+
+#include "Test.h"
+
+void Test::onTick() {
+
+}
