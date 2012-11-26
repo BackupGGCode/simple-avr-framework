e763c6f99788a9750e3976729cde5d113213e6b2
radomir@mazon.pl <radomir@mazon.pl@9dec7959-b12e-a28f-30bc-18173cd9f915>
2012-11-26 22:40:56 +0000

    SAF2 wersja 2.0

---
+++ tags/Tag_SAF2-v2_0/SAF/main.c
@@ -0,0 +1,40 @@
+/*
+ * main.c
+ *
+ *  Created on: 21-11-2012
+ *      Author: radomir mazon
+ */
+
+#include "saf2core.h"
+#include "Test.h"
+#include "adds/rscom.h"
+#include "adds/input.h"
+#include <util/delay.h>
+
+int main()
+{
+	//init...
+	saf_init();
+	input_add(_D, 2);
+	input_add(_D, 3);
+	input_add(_D, 4);
+	input_add(_D, 5);
+
+	//bindowanie...
+	saf_addEventHandler(onTestEvent);
+	saf_addEventHandler(rs_onEvent);
+	saf_addEventHandler(input_onEvent);
+
+
+	/**
+	 * Main. Gowna petla przekazuje steroanie do SAF.
+	 * Nie nalezy jej zmieniac...
+	 */
+	sei();
+	while(1) {
+		saf_process();
+		sleep_mode();
+	}
+}
+
+
