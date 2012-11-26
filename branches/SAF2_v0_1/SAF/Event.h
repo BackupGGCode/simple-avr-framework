a7e60a7b2d982eecca7d2b36c79d992f7047d2ef
radomir@mazon.pl <radomir@mazon.pl@9dec7959-b12e-a28f-30bc-18173cd9f915>
2012-11-26 13:04:45 +0000

    pierwsze starcie.. Nie testowane....

--- branches/SAF2_v0_1/SAF/Event.h
+++ branches/SAF2_v0_1/SAF/Event.h
@@ -1,9 +1,10 @@
 //Budowa event:
 //EVENT_doKogo_co
 
-#define EVENT_TEST_TEST		0
-#define EVENT_RS_SEND		1
-#define EVENT_RS_RECEIVE	2
+#define EVENT_NULL			0
+#define EVENT_SAFTICK		1
+#define EVENT_RS_SEND		2
+#define EVENT_RS_RECEIVE	3
 
 
 
