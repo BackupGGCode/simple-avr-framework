1d035b014187c48192b1b91aed5dd2b966261b6c
radomir@mazon.pl <radomir@mazon.pl@9dec7959-b12e-a28f-30bc-18173cd9f915>
2012-11-28 13:46:24 +0000

    versja 2.0

--- trunk/SAF/Event.h
+++ trunk/SAF/Event.h
@@ -1,10 +1,14 @@
 //Budowa event:
 //EVENT_doKogo_co
 
-#define EVENT_TEST_TEST		0
-#define EVENT_RS_SEND		1
-#define EVENT_RS_RECEIVE	2
+#define EVENT_NULL			0
+#define EVENT_SAFTICK		1
+#define EVENT_RS_SEND		2
+#define EVENT_RS_RECEIVE	3
+#define EVENT_BUTTON_DOWN	4
+#define EVENT_BUTTON_UP		5
 
+#define EVENT_TEST			6
 
 
 
