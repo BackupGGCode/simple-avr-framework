d3080339ede25503c7e01d5563fb54c160546f0d
radomir@mazon.pl <radomir@mazon.pl@9dec7959-b12e-a28f-30bc-18173cd9f915>
2012-12-09 14:13:46 +0000

    version 2.2

---
+++ tags/Tag_SAF2-v2_2/SAF/Event.h
@@ -0,0 +1,19 @@
+//Budowa event:
+//EVENT_doKogo_co
+
+#define EVENT_NULL			0
+#define EVENT_INIT			1
+#define EVENT_START_APP		2
+#define EVENT_SAFTICK		3
+#define EVENT_ERROR			4
+#define EVENT_RS_SEND		5
+#define EVENT_RS_RECEIVE	6
+#define EVENT_BUTTON_DOWN	7
+#define EVENT_BUTTON_UP		8
+#define EVENT_LCD_ON		9
+#define EVENT_LCD_OFF		10
+#define EVENT_LCD_GOTO		11
+#define EVENT_LCD_SEND		12
+#define EVENT_LCD_HOME1		13
+#define EVENT_LCD_HOME2		14
+#define EVENT_LCD_CLEAR		15
