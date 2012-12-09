6fd792fdbc7b1a75680a7116dc6211cf62750fed
radomir@mazon.pl <radomir@mazon.pl@9dec7959-b12e-a28f-30bc-18173cd9f915>
2012-12-09 14:09:22 +0000

    2.2

--- branches/SAF_v_2_2/SAF/saf2core.h
+++ branches/SAF_v_2_2/SAF/saf2core.h
@@ -12,14 +12,18 @@
 #include <avr/sleep.h>
 #include "Event.h"
 
-//-----------setup------------------------------------------------------
-#define SAF_LISTENER_SIZE 					8
+//-----------| SETUP |--------------------------------------------------
+#define SAF_LISTENER_SIZE 					4
 #define SAF_EVENT_BUFFOR_SIZE				32
 #define SAF_EVENT_VALUE_T					uint8_t
+#define SAF_TIMER_ENABLED					1
+#define SAF_TIMER_SIZE						4
+#define SAF_TICK_ENABLED					0
 #define SAF_CONFIG_EXTRA_EVENT_VALUE_ENABLE 0
 #define SAF_CONFIG_EXTRA_EVENT_VALUE1	//mozesz dodac dodatkowe pola struktury saf_Event
 #define SAF_CONFIG_EXTRA_EVENT_VALUE2	//przyklad
 #define SAF_CONFIG_EXTRA_EVENT_VALUE3	//int extraVal;
+
 //----------------------------------------------------------------------
 
 #if defined(EVENT_SAFTICK)
@@ -32,6 +36,15 @@
 #error "event EVENT_NULL is required (in event.h)";
 #endif
 
+#if defined(EVENT_START_APP)
+#else
+#error "event EVENT_START_APP is required (in event.h)";
+#endif
+
+//----------------
+//util
+#define sbi(port, bit) (port) |= (1 << (bit))
+#define cbi(port, bit) (port) &= ~(1 << (bit))
 //----------------
 typedef struct
 {
@@ -61,7 +74,10 @@ typedef struct
 {
 	_saf_EventListener list;
 	_saf_RingBuffer buffer;
+#if SAF_TICK_ENABLED == 1
 	SAF_EVENT_VALUE_T timeCounter;
+#endif
+	uint8_t startApp;
 } SAF;
 
 void saf_init();
@@ -81,6 +97,18 @@ void _saf_ringbufferFlush();
 //helper
 #if _SAF_CONFIG_EXTRA_EVENT_VALUE_ENABLE == 0
 	void saf_eventBusSend_(uint8_t code, SAF_EVENT_VALUE_T value);
+#endif
+
+//timer
+#if SAF_TIMER_ENABLED == 1
+	typedef struct {
+		uint8_t eventCode;
+		uint16_t couter;
+		uint8_t value;
+	} _saf_timer_t;
+	void saf_startTimer(uint16_t interval, uint8_t eventCode, uint8_t value);
+	void _saf_timerProcess();
+	void _saf_timerInit();
 #endif
 
 #endif /* SAF2CORE_H_ */
