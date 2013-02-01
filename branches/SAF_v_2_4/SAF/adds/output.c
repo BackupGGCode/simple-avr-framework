35f55ea32ed1ec316e6edda69952bfcf6d0aabe7
radomir@mazon.pl <radomir@mazon.pl@9dec7959-b12e-a28f-30bc-18173cd9f915>
2013-02-01 09:55:06 +0000

    blink

--- branches/SAF_v_2_4/SAF/adds/output.c
+++ branches/SAF_v_2_4/SAF/adds/output.c
@@ -6,37 +6,60 @@
  */
 #include "output.h"
 
-#define getSFR(i) _output_table.sfr[i]
-#define getBit(i) (_output_table.bit[i]&0x0F)
-#define getState(i) ((_output_table.bit[i]&0xF0)>>4)
+#define _out_getSFR(i) _out_table.sfr[i]
+#define _out_getBit(i) (_out_table.bit[i]&0x0F)
+#define out_getState(i) ((_out_table.bit[i]&0xF0)>>4)
 
-#define setSFR(i,v) _output_table.sfr[i]=v
-#define setBit(i,v) _output_table.bit[i]=v
-#define setState(i,v) _output_table.bit[i] = (_output_table.bit[i]&0x0F) | (v<<4)
+#define _out_setSFR(i,v) _out_table.sfr[i]=v
+#define _out_setBit(i,v) _out_table.bit[i]=v
+#define _out_setState(i,v) _out_table.bit[i] = (_out_table.bit[i]&0x0F) | (v<<4)
 
-#define setLo(i) _SFR_IO8(getSFR(i) + _PORT) &= ~(1<<getBit(i));
-#define setHi(i) _SFR_IO8(getSFR(i) + _PORT) |= 1<<getBit(i);
+#define _out_setLo(i) _SFR_IO8(_out_getSFR(i) + _PORT) &= ~(1<<_out_getBit(i));
+#define _out_setHi(i) _SFR_IO8(_out_getSFR(i) + _PORT) |= 1<<_out_getBit(i);
 
-_io_type _output_table;
-uint8_t _output_index = 0;
+#define FREE_ITEM 0xff
+#define _DDR 1
+#define _PORT 2
+
+/**
+ * Stan zostal zapisany w tablicy bit pod maska 0xF0
+ */
+typedef struct {
+	uint8_t bit[OUTPUT_SIZE];
+	uint8_t sfr[OUTPUT_SIZE];
+} _io_type;
+
+_io_type _out_table;
+uint8_t _out_index = 0;
+uint8_t _out_blink[OUTPUT_BLINK_SIZE];
+
+#if OUTPUT_BLINK_LOOP_CTRL == 1
+uint8_t _loopCounter[OUTPUT_BLINK_LOOP_CTRL_SIZE];
+#endif
+
 
 void output_add(uint8_t sfr, uint8_t bit, uint8_t isNegative) {
-	setSFR(_output_index, sfr);
-	setBit(_output_index, bit);
-	setState(_output_index, isNegative);
-	_output_index++;
+	_out_setSFR(_out_index, sfr);
+	_out_setBit(_out_index, bit);
+	_out_setState(_out_index, isNegative);
+	_out_index++;
 	_output_setup(isNegative);
 }
 
 void _output_setup() {
-	for (uint8_t i=0; i<_output_index; i++) {
+#if OUTPUT_BLINK_LOOP_CTRL == 1
+	for (uint8_t i=0; i<OUTPUT_BLINK_LOOP_CTRL_SIZE; i++) {
+		_loopCounter[i] = FREE_ITEM;
+	}
+#endif
+	for (uint8_t i=0; i<_out_index; i++) {
 		//DDR
-		_SFR_IO8(getSFR(i) + _DDR) |= 1<<getBit(i);
+		_SFR_IO8(_out_getSFR(i) + _DDR) |= 1<<_out_getBit(i);
 		//state
-		if (getState(i) == 0) {
-			setLo(i);
+		if (out_getState(i) == 0) {
+			_out_setLo(i);
 		} else {
-			setHi(i);
+			_out_setHi(i);
 		}
 
 	}
@@ -44,43 +67,69 @@ void _output_setup() {
 
 void output_onEvent(saf_Event event) {
 	if (event.code == EVENT_OUT_DOWN) {
-		if (event.value < _output_index){
-				if (getState(event.value) == 0) {
-					setLo(event.value);
+		if (event.value < _out_index){
+				if (out_getState(event.value) == 0) {
+					_out_setLo(event.value);
 				} else {
-					setHi(event.value);
+					_out_setHi(event.value);
 				}
 		}
 	}
 
 	if (event.code == EVENT_OUT_UP) {
-		if (event.value < _output_index){
-			if (getState(event.value) == 0) {
-				setHi(event.value);
+		if (event.value < _out_index){
+			if (out_getState(event.value) == 0) {
+				_out_setHi(event.value);
 			} else {
-				setLo(event.value);
+				_out_setLo(event.value);
 			}
 		}
 	}
 
 	if (event.code == EVENT_OUT_BLINK) {
-		saf_eventBusSend_(EVENT_OUT_UP, event.value);
-		saf_startTimer(OUTPUT_BLINK_DELAY, EVENT_OUT_BLINK_END, event.value);
+		uint16_t interval = OUTPUT_BLINK_DELAY*(event.value >> 5);
+		if (interval == 0) {
+			interval = 1;
+		}
+		saf_eventBusSend_(EVENT_OUT_UP, event.value & 0b00011111);
+		saf_startTimer(interval*2, EVENT_OUT_DOWN, event.value & 0b00011111);
 	}
 
-	if (event.code == EVENT_OUT_BLINK_END) {
-		saf_eventBusSend_(EVENT_OUT_DOWN, event.value & ~0x80);
-		if ((event.value & 0x80) > 0) {
-			saf_startTimer(OUTPUT_BLINK_DELAY, EVENT_OUT_BLINK2, event.value);
+#if OUTPUT_BLINK_LOOP_CTRL == 1
+	if (event.code == EVENT_OUT_BLINK_LOOP) {
+		uint16_t interval = OUTPUT_BLINK_DELAY*(event.value >> 5) + 1;
+		uint8_t value = event.value & 0b00011111;
+		saf_eventBusSend_(EVENT_OUT_UP, value);
+		saf_startTimer(interval, EVENT_OUT_DOWN, value);
+		saf_startTimer(interval*2, EVENT_OUT_BLINK_LOOP, event.value);
+
+		uint8_t index = 0xFF;
+		for (uint8_t i=0; i<OUTPUT_BLINK_LOOP_CTRL_SIZE; i++) {
+			if (_loopCounter[i] == FREE_ITEM) {
+				index = i;
+			}
+			if ((_loopCounter[i]&0b00011111) == value) {
+				index = i;
+				break;
+			}
 		}
+		if (index!=0xFF) {
+			_loopCounter[index] = event.value;
+		}
+
 	}
 
-	if (event.code == EVENT_OUT_BLINK2) {
-		saf_eventBusSend_(EVENT_OUT_UP, event.value & ~0x80);
-		if ((event.value & 0x80) > 0) {
-			saf_startTimer(OUTPUT_BLINK_DELAY, EVENT_OUT_BLINK_END, event.value & ~0x80);
-		} else {
-			saf_startTimer(OUTPUT_BLINK_DELAY, EVENT_OUT_BLINK_END, event.value | 0x80);
+	if (event.code == EVENT_OUT_BLINK_STOP) {
+		uint8_t value = event.value & 0b00011111;
+		for (uint8_t i=0; i<OUTPUT_BLINK_LOOP_CTRL_SIZE; i++) {
+			if ((_loopCounter[i]&0b00011111) == value) {
+				saf_cancelTimer(EVENT_OUT_DOWN, value);
+				saf_cancelTimer(EVENT_OUT_BLINK_LOOP, _loopCounter[i]);
+				_loopCounter[i] = FREE_ITEM;
+				saf_eventBusSend_(EVENT_OUT_DOWN, value);
+				break;
+			}
 		}
 	}
+#endif
 }
--- branches/SAF_v_2_4/SAF/adds/output.h
+++ branches/SAF_v_2_4/SAF/adds/output.h
@@ -2,20 +2,54 @@
  * output.h
  *
  *  Created on: 17-12-2012
- *      Author: Administrator
+ *      Author: Radomir Mazon
+ *
+ *      Rejestracja pinow wyjsciowych : output_add() gdzie:
+ *      stf - to zmienna okreslajaca port: _B, _C, _D
+ *      bit - numer bitu w porcie
+ *      isNegative - domyslna wartoscia poczatkowa portu jest 0.
+ *      			jesli chcemy to zmienic isNegative powinno byc > 0.
+ *
+ *      Eventy wysylane przez modul:
+ *      EVENT_OUT_BLINK_STOP	- value zawiera numer, ktory nalezy zakonczyc miganie (tylko jesli wczesniej
+ *      						dla tego numeru miganie zostalo wlaczone przez EVENT_OUT_BLINK_LOOP
+ *      Eventy przyjmowane przez modul: (value zawiera numer wyjscia)
+ *      EVENT_OUT_DOWN	- Zapis do bitu portu 0 (lub 1 w przypadku isNegative>0)
+ *		EVENT_OUT_UP	- Zapis do bitu portu 1 (lub 0 w przypadku isNegative>0)
+ *		EVENT_OUT_BLINK	- Pojedyncze migniecie. Czas zapisany jest w masce 0b11100000.
+ *		EVENT_OUT_BLINK_LOOP - Migniecie z automatycznym odnawianiem (miganie do momentu odebrania EVENT_OUT_BLINK_STOP).
+ *								Czas zapisany jest w masce 0b11100000.
+ *
+ *		UWAGA: numer wyjscia to numer indeksu liczonego od 0 w kolejnosci rejestrowania wyjscia przez output_add()
  */
-
 #ifndef OUTPUT_H_
 #define OUTPUT_H_
 
 #include "../saf2core.h"
-#include "input.h"
 
-#define OUTPUT_SIZE 8
-#define OUTPUT_BLINK_DELAY 255
+//------------- setup
+#define OUTPUT_SIZE					8
+//DELAY max 4095
+#define OUTPUT_BLINK_DELAY			255
+#define OUTPUT_BLINK_SIZE			4
+#define OUTPUT_BLINK_LOOP_CTRL		1
+#define OUTPUT_BLINK_LOOP_CTRL_SIZE SAF_TIMER_SIZE/2
 
+//-------------------
+/**
+ * byte dla EVENT_OUT_BLINK i EVENT_OUT_BLINK_LOOP
+ * bit7-5 = mnożnik OUTPUT_BLINK_DELAY (wartosc od 0 do 7)
+ * bit4-0 = id wyjscia dodanego output_add() (wartosc od 0 do 31)
+ */
+#define outputBlink(delay, item) (uint8_t)((delay<<5) | item)
 void output_add(uint8_t sfr, uint8_t bit, uint8_t isNegative);
 void _output_setup();
 void output_onEvent(saf_Event event);
 
+#if SAF_TIMER_ENABLED == 0
+#error "SAF timer is required !"
+#endif
+#if SAF_TICK_ENABLED == 0
+#error "SAF tick is required !"
+#endif
 #endif /* OUTPUT_H_ */
