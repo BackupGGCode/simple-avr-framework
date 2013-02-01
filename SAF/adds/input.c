c592b9c31ba909821c60bffcbe4d7a87fe73f785
radomir@mazon.pl <radomir@mazon.pl@9dec7959-b12e-a28f-30bc-18173cd9f915>
2013-02-01 09:55:06 +0000

    blink

--- SAF/adds/input.c
+++ SAF/adds/input.c
@@ -7,42 +7,54 @@
 
 #include "input.h"
 
-#define getSFR(i) _input_table.sfr[i]
-#define getBit(i) (_input_table.bit[i]&0x0F)
-#define getState(i) ((_input_table.bit[i]&0xF0)>>4)
+#define _io_getSFR(i) _input_table.sfr[i]
+#define _io_getBit(i) (_input_table.bit[i]&0x0F)
+#define io_getState(i) ((_input_table.bit[i]&0xF0)>>4)
 
-#define setSFR(i,v) _input_table.sfr[i]=v
-#define setBit(i,v) _input_table.bit[i]=v
-#define setState(i,v) _input_table.bit[i] = (_input_table.bit[i]&0x0F) | (v<<4)
+#define _io_setSFR(i,v) _input_table.sfr[i]=v
+#define _io_setBit(i,v) _input_table.bit[i]=v
+#define _io_setState(i,v) _input_table.bit[i] = (_input_table.bit[i]&0x0F) | (v<<4)
+
+#define _DDR 1
+#define _PORT 2
+
+/**
+ * Stan zostal zapisany w tablicy bit pod maska 0xF0
+ */
+typedef struct {
+	uint8_t bit[INPUT_SIZE];
+	uint8_t sfr[INPUT_SIZE];
+} _io_type;
 
 _io_type _input_table;
 uint8_t _input_index = 0;
 
-void input_add(uint8_t sfr, uint8_t bit) {
-	setSFR(_input_index, sfr);
-	setBit(_input_index, bit);
+uint8_t input_add(uint8_t sfr, uint8_t bit) {
+	_io_setSFR(_input_index, sfr);
+	_io_setBit(_input_index, bit);
 	_input_index++;
 	_input_setup();
+	return _input_index-1;
 }
 
 void _input_setup() {
 	for (uint8_t i=0; i<_input_index; i++) {
 		//DDR
-		_SFR_IO8(getSFR(i) + _DDR) &= ~(1<<getBit(i));
+		_SFR_IO8(_io_getSFR(i) + _DDR) &= ~(1<<_io_getBit(i));
 		//PULL-UP
-		_SFR_IO8(getSFR(i) + _PORT) |= 1<<getBit(i);
+		_SFR_IO8(_io_getSFR(i) + _PORT) |= 1<<_io_getBit(i);
 		//state
-		setState(i, bit_is_clear(_SFR_IO8(getSFR(i)), getBit(i)));
+		_io_setState(i, bit_is_clear(_SFR_IO8(_io_getSFR(i)), _io_getBit(i)));
 	}
 }
 
 void input_onEvent(saf_Event event) {
-	if (event.code == EVENT_SAFTICK) {
+	if (event.code == EVENT_SAFTICK && (event.value%4) == 0) {
 		for (uint8_t i=0; i<_input_index; i++) {
-			uint8_t curState = bit_is_clear(_SFR_IO8(getSFR(i)), getBit(i));
-			if (getState(i) != curState)
+			uint8_t curState = bit_is_clear(_SFR_IO8(_io_getSFR(i)), _io_getBit(i));
+			if (io_getState(i) != curState)
 			{
-				setState(i, curState);
+				_io_setState(i, curState);
 				saf_Event newEvent;
 				newEvent.value = i;
 				if (curState == 1) {
